90f8f3f420e899b9ed2896f1fc513d45efee335c
NguyenTaiHCMUS <163984153+NDT0109@users.noreply.github.com>
2024-10-26 10:39:59 +0700

    Delete 02 - Circuits/1_Combinational Logic/1_Basic Gates directory

--- 02 - Circuits/1_Combinational Logic/1_Basic Gates/13_Ring or vibrate.v
+++
@@ -1,9 +0,0 @@
-module top_module (
-    input ring,
-    input vibrate_mode,
-    output ringer,       // Make sound
-    output motor         // Vibrate
-);
-    assign ringer = (vibrate_mode)?1'b0:ring;
-    assign motor = (ring==1&&vibrate_mode==1)?vibrate_mode:1'b0;
-endmodule
